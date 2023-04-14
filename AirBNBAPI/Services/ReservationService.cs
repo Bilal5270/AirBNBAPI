f297ba743fc828c3f03178658e5a31dda061e25a
Bilal Yousef <s1169284@student.windesheim.nl>
2023-04-14 11:48:16 +0200

    added some comments to explain possible conflict

--- AirBNBAPI/Services/ReservationService.cs
+++ AirBNBAPI/Services/ReservationService.cs
@@ -40,6 +40,14 @@ namespace AirBNBAPI.Services
             reservation.Location = location;
 
             // Heb een extra validatie toegevoegd..
+            // Ik heb een probleem gevonden met de front-end.
+            // Het lijkt erop dat als je een datum aanklikt in de datepicker dat hij de startdatum en einddatum een dag eerder pakt.
+            // Hierdoor geeft hij onjuiste waardes aan naar mijn POST request.
+            // Normaal gesproken is dit geen probleem, echter heb ik een conflict checker aangemaakt die dus wel nu op de bestaande reserveringen controleerd.
+            // Omdat die nu een dag eerder zijn botst die met mijn conflict checker. Ik snap dat het nu al de dag van de deadline is en dat dit niet gefixt kan worden.
+            // Kan hier echter wel rekening mee gehouden worden voor mijn conflict checker?
+            // Als je conflicten hebt zet of deze extra check uit, of reserveer in de front-end 1 dag later dan de laatste unavailable date in de date picker.
+            //Dus als 10 mei is gereserveerd in de datepicker, dan kan je pas vanaf 12 mei in de front-end een reservering maken zonder conflict.
             var existingReservations = await _airBnBRepository.GetExistingReservationsAsync(reservation.LocationId, reservation.StartDate, reservation.EndDate, cancellationToken);
             if (existingReservations.Any())
             {
